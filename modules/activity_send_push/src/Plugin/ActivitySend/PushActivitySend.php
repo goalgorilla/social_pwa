ad53506ed7865ee597b1f2901ce3d06288bd2693
Ronald te Brake <ronald@goalgorilla.com>
2017-03-24 14:02:55 +0100

    Message stack integration

---
+++ modules/activity_send_push/src/Plugin/ActivitySend/PushActivitySend.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\activity_send_push\Plugin\ActivitySend\PushActivitySend.
+ */
+
+namespace Drupal\activity_send_push\Plugin\ActivitySend;
+
+use Drupal\activity_send\Plugin\ActivitySendBase;
+
+/**
+ * Provides a 'PushActivitySend' activity action.
+ *
+ * @ActivitySend(
+ *  id = "push_activity_send",
+ *  label = @Translation("Action that is triggered when a entity is created"),
+ * )
+ */
+class PushActivitySend extends ActivitySendBase {
+
+  /**
+   * @inheritdoc
+   */
+  public function create($entity) {
+    $data['entity_id'] = $entity->id();
+    $queue = \Drupal::queue('activity_send_email_worker');
+    $queue->createItem($data);
+  }
+
+}
